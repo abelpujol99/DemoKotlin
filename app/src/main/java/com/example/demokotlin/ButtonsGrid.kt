c2f472f834e8377c8f0233b1b349b317c549053d
abel pujol <abelpujol99@gmail.com>
2023-10-10 11:03:11 +0200

    Calculator

---
+++ app/src/main/java/com/example/demokotlin/ButtonsGrid.kt
@@ -0,0 +1,58 @@
+package com.example.demokotlin
+
+import android.content.Context
+import android.widget.Button
+import android.widget.LinearLayout
+
+class ButtonsGrid(val context: Context, val parentLinearLayout: LinearLayout) {
+
+    class ButtonsRow(val context: Context, val parentLinearLayout: LinearLayout){
+
+        private var buttons: MutableList<Button> = mutableListOf();
+        private val linearLayout: LinearLayout = LinearLayout(context);
+
+        init {
+            linearLayout.orientation = LinearLayout.HORIZONTAL;
+            linearLayout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1f
+            )
+            linearLayout.setBackgroundColor(context.getColor(R.color.teal_700))
+
+            parentLinearLayout.addView(linearLayout);
+        }
+
+        fun AddButton(text: String) : Button{
+
+            var button = Button(context)
+            button.text = text;
+            button.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1f
+            )
+            linearLayout.addView(button);
+            buttons.add(button)
+            return button;
+        }
+
+    }
+
+    private var rows: MutableList<ButtonsRow> = mutableListOf();
+
+    fun GetRow(index: Int) : ButtonsRow? {
+
+        if (index >= rows.count() || index < 0){
+            return null;
+        }
+        return rows[index];
+    }
+
+    fun AddRow() : ButtonsRow{
+
+        val buttonRow = ButtonsRow(context, parentLinearLayout);
+        rows.add(buttonRow);
+        return  buttonRow;
+    }
+}
--- app/src/main/java/com/example/demokotlin/MainActivity.kt
+++ app/src/main/java/com/example/demokotlin/MainActivity.kt
@@ -1,31 +1,119 @@
 package com.example.demokotlin
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.core.widget.doAfterTextChanged
-import androidx.core.widget.doBeforeTextChanged
-
 
 class MainActivity : ComponentActivity() {
 
-    val startText: TextView by lazy { findViewById(R.id.startScreenText) }
-    val startButton: Button by lazy { findViewById(R.id.startScreenButton) }
-    val startEditText: EditText by lazy { findViewById(R.id.startScreenEditText) }
+    val result: TextView by lazy { findViewById(R.id.result) }
+    val buttonsContainer: LinearLayout by lazy { findViewById(R.id.buttons_container) }
 
-    var counter = 0
+    enum class Operation(private val operation: (a: Int, b:Int) -> Int, val operationCharacter : String){
+        Add({a, b -> a + b}, " + "),
+        Sub({a, b -> a - b}, " - "),
+        Mul({a, b -> a * b}, " x "),
+        Div({a, b -> a / b}, " / ");
 
+        public fun Operate(a: Int, b : Int) : Int {
+            return this.operation(a, b);
+        }
+    }
+
+    var A : Int? = null;
+    var B : Int? = null;
+    var operation : Operation? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
 
-        startButton.setOnClickListener{
-            counter++
-            startText.text = "holi $counter"
+        val buttonsGrid = ButtonsGrid(this, buttonsContainer);
+
+        var names: MutableList<MutableList<String>> = mutableListOf()
+
+        names.add(mutableListOf("AC", "()", "%", "/"))
+        names.add(mutableListOf("7", "8", "9", "X"))
+        names.add(mutableListOf("4", "5", "6", "-"))
+        names.add(mutableListOf("1", "2", "3", "+"))
+        names.add(mutableListOf("0", ".", "<-", "="))
+
+        for (i in names.indices){
+            val row = buttonsGrid.AddRow();
+
+            for (j in names[i].indices){
+                val button = row.AddButton(names[i][j]);
+
+                button.setOnClickListener{
+                    when(names[i][j]){
+                        "AC" -> {
+                            A = null;
+                            B = null;
+                            operation = null;
+                            result.text = "0";
+                        }
+
+                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {
+                            NumberPress(names[i][j].toInt())
+                        }
+
+                        "+" -> {
+                            OperationPress(Operation.Add)
+                        }
+                        "-" -> {
+                            OperationPress(Operation.Sub)
+                        }
+                        "X" -> {
+                            OperationPress(Operation.Mul)
+                        }
+                        "/" -> {
+                            OperationPress(Operation.Div)
+                        }
+                        "=" -> {
+                            A?.let { a ->
+                                B?.let { b ->
+                                    val resultNum = operation?.Operate(a, b);
+                                    result.text = resultNum.toString();
+                                    A = resultNum;
+                                    B = null;
+                                    operation = null;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    fun NumberPress(num : Int){
+        if (operation == null){
+            //Cosas de A
+            A?.let { a ->
+                A = (a * 10) + num
+            } ?: run {
+                A = num;
+            }
+            result.text = A.toString()
+        }
+        else
+        {
+            //Cosas de B
+            B?.let { b ->
+                B = (b * 10) + num
+            } ?: run {
+                B = num;
+            }
+            result.text = A.toString() + operation?.operationCharacter + B.toString();
+        }
+    }
+
+    fun OperationPress(operation: Operation){
+        if (A != null && B == null){
+            this.operation = operation;
+            result.text = A.toString() + operation?.operationCharacter;
         }
     }
 }
